1294df73805ae5e34ca6006fb52577f745df8355
Anderson <30065845+azzevedo@users.noreply.github.com>
2022-05-04 14:00:49 +0000

    Mudança para POO

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from automata import Marvin
+
+
+
+marvin = Marvin()
+marvin.setupDriver()
+marvin.twitterLogin()
+marvin.youLikeHitsLogin()
+marvin.confirmLogin()
+marvin.twitterLikes()
+marvin.twitterFollow()
+marvin.showEarnedPoints()
+
+# marvin.bonusPoints()
+
+# TODO
+# Implementar captura de KeyboardInterrupt
+# Para finalizar o programa
+
+#TODO apagar linha abaixo caso seja pra finalizar com ctrl + c
+input("Enter para finalizar!")
+########   Fechar o navegador   ########
+marvin.goodbye()
